cfc818ddc8e6d75ca9d4c3f705c6d302f97ac16e
aprybysh <alekspribysh@gmail.com>
2016-01-26 16:44:25 -0800

    Initial commit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+
+    private int quantity = 0;
+    public void submitOrder(View view) {
+
+        displayPrice(quantity * 5);
+    }
+
+    public void plusCup(View view){
+        quantity = quantity+1;
+        display(quantity);
+    }
+    public void minusCup(View view){
+        if(quantity > 0){
+        quantity = quantity-1;
+            display(quantity);}
+        else {
+            display(quantity);
+        }
+
+    }
+
+
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView) findViewById(
+                R.id.price_text_view
+        );
+        priceTextView.setText("$ "+number);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+}
